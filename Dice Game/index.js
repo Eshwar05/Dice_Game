485a280cf21801e49fa4be76c2488056916aaf51
Eshwar05 <116936940+Eshwar05@users.noreply.github.com>
2023-06-24 19:50:31 +0530

    Add files via upload

---
+++ Dice Game/index.js
@@ -0,0 +1,25 @@
+function playgame(){
+var radNo1=Math.floor(Math.random() * 6)+1;
+var radDiceImg="dice"+radNo1+".png";
+var radImgSor="images/"+radDiceImg;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",radImgSor)
+
+
+var radNo2=Math.floor(Math.random() * 6)+1;
+var radDiceImg="dice"+radNo2+".png";
+var radImgSor2="images/"+radDiceImg;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",radImgSor2)
+
+
+if(radNo1 > radNo2){
+    document.getElementById("play").innerHTML="🚩Play 1 Wins!";
+}
+else if(radNo1<radNo2){
+    document.getElementById("play").innerHTML="Play 2 Wins!🚩";
+}
+else{
+    document.getElementById("play").innerHTML="Draw!";
+}
+}
